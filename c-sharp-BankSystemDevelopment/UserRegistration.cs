bd85301834be35300ff3f0b3f706208e4f7d3e1b
omranali2111 <aljabri1234@hotmail.com>
2023-09-10 10:10:59 +0400

    adding the user properties and the methods to register the user

--- c-sharp-BankSystemDevelopment/UserRegistration.cs
+++ c-sharp-BankSystemDevelopment/UserRegistration.cs
@@ -2,13 +2,65 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
 
 namespace c_sharp_BankSystemDevelopment
 {
     internal class UserRegistration
     {
+        private List<User> registeredUsers = new List<User>();
        
+        private void RegisterUser()
+        {
+            Console.WriteLine("Enter your name: ");
+            string name = Console.ReadLine();
 
+            Console.WriteLine("Enter your email: ");
+            string email = Console.ReadLine();
+
+            if (IsEmailUnique(email))
+            {
+                Console.WriteLine("Enter your password: ");
+                string password = Console.ReadLine();
+
+                
+
+                User newUser = new User(name, email, password);
+                registeredUsers.Add(newUser);
+                Save();
+                Console.WriteLine("Registration successful!");
+            }
+            else
+            {
+                Console.WriteLine("Email address is already registered.");
+            }
+        }
+        public void Save()
+        {
+            try
+            {
+                string json = JsonSerializer.Serialize(this, new JsonSerializerOptions { WriteIndented = true });
+                File.WriteAllText($"Users.json", json);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"An error occurred while saving the invoice: {ex.Message}");
+            }
+        }
+
+        private bool IsEmailUnique(string email)
+        {
+            foreach (User user in registeredUsers)
+            {
+                if (user.Email == email)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
     }
+
 }
+
