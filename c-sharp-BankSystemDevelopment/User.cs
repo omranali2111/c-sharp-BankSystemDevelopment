b4a76e486702ac33250a6f00429efdff5cbb97d8
omranali2111 <aljabri1234@hotmail.com>
2023-09-10 09:56:15 +0400

    add class user and user registration

---
+++ c-sharp-BankSystemDevelopment/User.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace c_sharp_BankSystemDevelopment
+{
+    internal class User
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+
+        public User(string name, string email, string password)
+        {
+            Name = name;
+            Email = email;
+            Password = password;
+        }
+    }
+}
---
+++ c-sharp-BankSystemDevelopment/UserRegistration.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace c_sharp_BankSystemDevelopment
+{
+    internal class UserRegistration
+    {
+       
+
+    }
+}
